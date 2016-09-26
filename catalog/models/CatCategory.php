62770a56f9d93cb1be1ee7b1982fb0243a521b55
begemot <scott2to@gmail.com>
2016-09-26 17:22:48 +0200

    Порядок подкатегорий в каталоге

--- catalog/models/CatCategory.php
+++ catalog/models/CatCategory.php
@@ -82,6 +82,7 @@ class CatCategory extends CActiveRecord
 		// NOTE: you may need to adjust the relation name and the related
 		// class name for the relations automatically generated below.
 		return array(
+            'childCategories' => array(self::HAS_MANY, 'CatCategory', 'pid','order'=>'`childCategories`.`order` ASC'),
 		);
 	}
 
