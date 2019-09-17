18531359e495d73a95d8afdd1f15b2a7f10dba45
root <scott2to@gmail.com>
2019-09-17 15:49:33 +0200

    мог только админ, теперь суперпользователь

--- seo/controllers/WebParserController.php
+++ seo/controllers/WebParserController.php
@@ -37,7 +37,7 @@ class WebParserController extends Controller
             ),
             array('allow', // allow admin user to perform 'admin' and 'delete' actions
                 'actions' => array('admin', 'delete', 'tagProcess','linkToPage','tagsDataCount','getTagFields','loadTagsData','unprocessedTagTaskCount'),
-                'users' => array('admin'),
+                'expression' => 'Yii::app()->user->canDo("")'
             ),
             array('deny',  // deny all users
                 'users' => array('*'),
