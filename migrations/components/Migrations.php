7164c47933ef920dd25cefd559aed99cd1ece1ed
root <scott2to@gmail.com>
2019-09-19 14:37:20 +0200

    Упаковал миграции связанные с seo_pages таблицей в одну. Сделал базовую функцию для проверки таблицы на существование.

--- migrations/components/Migrations.php
+++ migrations/components/Migrations.php
@@ -1,37 +1,49 @@
 <?php
 
-class Migrations{
-	public function isConfirmed($returnBoolean = false){
-		return "Неизвестно";
-	}
+class Migrations
+{
+    public function isConfirmed($returnBoolean = false)
+    {
+        return "Неизвестно";
+    }
 
-	public function getDescription()
-	{
-		return "Нету описания";
-	}
-	public function up()
-	{
-		return false;
-	}
+    public function getDescription()
+    {
+        return "Нету описания";
+    }
 
-	public function down()
-	{
-		return false;
-	}
+    public function up()
+    {
+        return false;
+    }
 
-	public function execute($sql)
-	{
-		return Yii::app()->db->createCommand($sql)->execute();
-	}
+    public function down()
+    {
+        return false;
+    }
 
-	public function confirmByWords($return)
-	{
-		if($return == true){
-			return "Применена";
-		}
-		else{
-			return "Еще не применялась";
-		}
+    public function execute($sql)
+    {
+        return Yii::app()->db->createCommand($sql)->execute();
+    }
 
-	}
+    public function confirmByWords($return)
+    {
+        if ($return == true) {
+            return "Применена";
+        } else {
+            return "Еще не применялась";
+        }
+
+    }
+
+    public function tableExist($tableName)
+    {
+        Yii::app()->db->schema->refresh();
+        $table = Yii::app()->db->schema->getTable('seo_pages');
+        if ($table) {
+            return true;
+        } else
+            return false;
+    }
 }
