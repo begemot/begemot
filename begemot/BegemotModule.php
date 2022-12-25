8bc0ce8f0d3c286f01756d8be623f585c325dabc
scott2to <scott2to@gmail.com>
2022-12-25 22:06:50 +0300

    убрал лишние import

--- begemot/BegemotModule.php
+++ begemot/BegemotModule.php
@@ -24,17 +24,7 @@ class BegemotModule extends CWebModule
 		// you may place code here to customize the module or the application
 
 		// import the module-level models and components
-		$this->setImport(array(
-			'begemot.models.*',
-			'begemot.components.*',
-			'jobs.components.*'
-		));
-        $component=Yii::createComponent(array(
 
-            'class'=>'begemot.extensions.bootstrap.components.Bootstrap'
-
-        ));
-        Yii::app()->setComponent('bootstrap',$component);
 	}
 
 	public function beforeControllerAction($controller, $action)
