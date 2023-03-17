1c21d04c95dfdee71cb66b1a9a53848ff16c67df
scott2to <scott2to@gmail.com>
2023-03-17 15:26:32 +0300

    добавил проверку корректности файла изображения как базовую функцию базового фильтра

--- pictureBox/components/filters/BaseFilter.php
+++ pictureBox/components/filters/BaseFilter.php
@@ -18,6 +18,19 @@ class BaseFilter{
         
 
     }
-    
+    public function checkImageformat($file){
+        try {
+            $image_info = @getimagesize($this->fileName);
+        } catch (Exception $e) {
+            return false;
+        }
+
+
+
+        if ($image_info == false){
+            return false;
+        }
+        return true;
+    }
 }
 ?>
--- pictureBox/components/filters/CropResizeFilter.php
+++ pictureBox/components/filters/CropResizeFilter.php
@@ -3,7 +3,12 @@ class CropResizeFilter extends BaseFilter{
        
     public function make (){
 
-        $im = new Imagick($this->fileName);
+        if ($this->checkImageformat($this->fileName)) {
+
+            $im = new Imagick($this->fileName);
+        } else return false;
+
+
         $im->resizeImage ($this->param['width'],$this->param['height'],2,0.9);
         $im->writeImage($this->newFileName);
         $im->clear();
