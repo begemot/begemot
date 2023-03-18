a2831ffa6813a404ec5c1c859381b1535bdd17f1
scott2to <scott2to@gmail.com>
2023-03-18 10:08:11 +0300

    добавление нового типа Int в schema и функция конвертации SchemaString to SchemaInt

---
+++ schema/models/types/SchmTypeInt.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Николай Козлов
+ * Date: 12.02.2021
+ * Time: 12:30
+ */
+
+class SchmTypeInt extends CActiveRecord
+{
+    public function tableName()
+    {
+        return 'SchmTypeInt';
+    }
+
+    public function relations()
+    {
+
+        return array();
+    }
+
+    /**
+     * @return array customized attribute labels (name=>label)
+     */
+    public function attributeLabels()
+    {
+        return array(
+
+        );
+    }
+    public function rules()
+    {
+        // NOTE: you should only define rules for those attributes that
+        // will receive user inputs.
+        return array(
+            array('fieldDataId', 'numerical','integerOnly' => true),
+            array('value', 'length', 'max' => 200),
+
+        );
+    }
+    public function search()
+    {
+        // @todo Please modify the following code to remove attributes that should not be searched.
+
+        $criteria = new CDbCriteria;
+
+        //$criteria->compare('id', $this->id);
+        // $criteria->compare('name', $this->name, true);
+        // $criteria->compare('pid', $this->pid);
+
+        return new CActiveDataProvider($this, array(
+            'criteria' => $criteria,
+        ));
+    }
+    public static function model($className = __CLASS__)
+    {
+        return parent::model($className);
+    }
+}
