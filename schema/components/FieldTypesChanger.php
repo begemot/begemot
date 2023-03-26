7c8ab872ff862e1ddb767e5341c7989d55b401ef
scott2to <scott2to@gmail.com>
2023-03-26 18:41:42 +0300

    смена типа SchemaField с int to string

--- schema/components/FieldTypesChanger.php
+++ schema/components/FieldTypesChanger.php
@@ -24,4 +24,27 @@ class FieldTypesChanger
 
         }
     }
+
+    public static function intToString($fieldId){
+
+        $schemaField = SchemaField::model()->findByPk($fieldId);
+        $schemaFieldType = $schemaField->type;
+
+        //$schemaField->type = 'int';
+        //$schemaField->save();
+
+        $schemaDataArray = SchemaData::model()->findAllByAttributes(['fieldId'=>$schemaField->id]);
+        //echo count($schemaDataArray);
+        foreach ($schemaDataArray as $schemaData ){
+            /** @var $schemaData SchemaData*/
+
+            print_r($schemaData);
+//            print_r($schemaData->value);
+//            $oldValue = $schemaData->value;
+////            $schemaData->fieldType = 'Int';
+//            $schemaData->setData($oldValue,'String');
+
+        }
+    }
+
 }
