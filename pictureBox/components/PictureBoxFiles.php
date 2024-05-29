5a994e10edcb2c36120fc81dd192e03b48a9ddc6
Scott2to <scott2to@gmail.com>
2024-05-29 17:58:57 +0300

    добавил переменную phpdoc что бы  не ругалось на PBox и форматирование

--- pictureBox/components/PictureBoxFiles.php
+++ pictureBox/components/PictureBoxFiles.php
@@ -1,7 +1,11 @@
 <?php
 
 Yii::import('pictureBox.components.PBox');
-class PictureBoxFiles extends CWidget {
+/**
+ * @var PBox
+ */
+class PictureBoxFiles extends CWidget
+{
 
     //Идентификатор множестка
     //например:books
@@ -16,30 +20,33 @@ class PictureBoxFiles extends CWidget {
 
     public $theme = 'tiles';
 
-    public function init() {
+    public function init()
+    {
         if (!file_exists(Yii::app()->basePath . '/../files/pictureBox/')) {
-            mkDir(Yii::app()->basePath . '/../files/pictureBox/',777);
+            mkDir(Yii::app()->basePath . '/../files/pictureBox/', 777);
         }
     }
 
-    public function run() {
+    public function run()
+    {
 
-        $this->config = array_merge_recursive(self::getDefaultConfig(),$this->config);
+        $this->config = array_merge_recursive(self::getDefaultConfig(), $this->config);
 
-        $pBox = new PBox($this->id,$this->elementId);
+        $pBox = new PBox($this->id, $this->elementId);
         $pBox->setImagesRenderRules($this->config);
 
         $this->renderContent();
     }
 
-    public static function getDefaultConfig(){
+    public static function getDefaultConfig()
+    {
         $defaultConfig = array(
 
-            'nativeFilters'=>array(
-                'admin' =>true,
+            'nativeFilters' => array(
+                'admin' => true,
             ),
-            'filtersTitles'=>array(
-                'admin' =>'Системный',
+            'filtersTitles' => array(
+                'admin' => 'Системный',
 
             ),
             'imageFilters' => array(
@@ -61,16 +68,15 @@ class PictureBoxFiles extends CWidget {
 
 
 
-    protected function renderContent() {
-          $theme  =   'pictureBox.components.view.angularTiles';
-//        if ($this->theme=='default'){
-//            $theme = 'pictureBox.components.view.pictureBoxView';
-//        } else {
-//            $theme = 'pictureBox.components.view.'.$this->theme;
-//        }
+    protected function renderContent()
+    {
+        $theme  =   'pictureBox.components.view.angularTiles';
+        //        if ($this->theme=='default'){
+        //            $theme = 'pictureBox.components.view.pictureBoxView';
+        //        } else {
+        //            $theme = 'pictureBox.components.view.'.$this->theme;
+        //        }
 
-        $this->render($theme,array('id'=>$this->id,'elementId'=>$this->elementId,'config'=>$this->config));
-
+        $this->render($theme, array('id' => $this->id, 'elementId' => $this->elementId, 'config' => $this->config));
     }
 }
-?>
