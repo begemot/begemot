d353b28c89e7816eb8411f6def25903afed2a359
Scott2to <scott2to@gmail.com>
2024-06-04 21:37:53 +0300

    Отображение в модуле миграций только активных модулей. Добавление в angularjs сервис метода, который выдает исключительно активные модули.

--- migrations/views/default/migrationsManage.php
+++ migrations/views/default/migrationsManage.php
@@ -3,92 +3,91 @@ Yii::app()->clientScript->registerScriptFile('/protected/modules/modules/assets/
 ?>
 
 <script>
+var app = angular.module('migration', ['modules'])
 
-    var app = angular.module('migration', ['modules'])
+app.controller('ctrl', ['$scope', '$http', 'modulesService', function($scope, $http, modulesService) {
 
-    app.controller('ctrl', ['$scope', '$http', 'modulesService', function ($scope, $http, modulesService) {
+    $scope.modulesList = null
+    $scope.migrationList = null
 
-        $scope.modulesList = null
-        $scope.migrationList = null
+    $scope.nameOfNewMigration = null
+    $scope.loading = false;
 
-        $scope.nameOfNewMigration = null
-        $scope.loading = false;
+    modulesService.loadData().then(() => {
 
-        modulesService.loadData().then(() => {
+        $scope.modulesList = modulesService.getActiveModulesData()
 
-            $scope.modulesList = modulesService.getModulesList()
+        $scope.selectModule($scope.modulesList[0]);
 
-            $scope.selectModule($scope.modulesList[0]);
+    });
 
-        });
+    $scope.selectModule = (module) => {
+        $scope.activeModule = module
+        $scope.loading = true
+        $scope.getMigrationList(module)
+    }
 
-        $scope.selectModule = (module) => {
-            $scope.activeModule = module
-            $scope.loading = true
-            $scope.getMigrationList(module)
-        }
+    $scope.getMigrationList = (module) => {
+        $http({
+            method: 'GET',
+            url: '/migrations/default/getMigrationsList',
+            params: {
+                moduleName: module
+            }
+        }).then((response) => {
 
-        $scope.getMigrationList = (module) => {
+            $scope.migrationList = response.data
+            $scope.loading = false
+        })
+    }
+
+    $scope.createNewMigrationFile = () => {
+        if ($scope.nameOfNewMigration != null)
             $http({
                 method: 'GET',
-                url: '/migrations/default/getMigrationsList',
-                params: {moduleName: module}
-            }).then((response) => {
-
-                $scope.migrationList = response.data
-                $scope.loading = false
-            })
-        }
-
-        $scope.createNewMigrationFile = () => {
-            if ($scope.nameOfNewMigration != null)
-                $http({
-                    method: 'GET',
-                    url: '/migrations/default/newMigrationFile',
-                    params: {
-                        fileName: $scope.nameOfNewMigration,
-                        module: $scope.activeModule
-                    }
-                }).then(() => {
-                    $scope.nameOfNewMigration = null
-                    $scope.getMigrationList($scope.activeModule)
-                })
-        }
-
-        $scope.upMigration =(file)=>{
-            $http({
-                method:'GET',
-                url:'/migrations/default/upMigration',
-                params:{
-                    module: $scope.activeModule,
-                    fileName:file
+                url: '/migrations/default/newMigrationFile',
+                params: {
+                    fileName: $scope.nameOfNewMigration,
+                    module: $scope.activeModule
                 }
-            }).then(()=>{
+            }).then(() => {
+                $scope.nameOfNewMigration = null
                 $scope.getMigrationList($scope.activeModule)
             })
-        }
-        $scope.downMigration =(file)=>{
-            $http({
-                method:'GET',
-                url:'/migrations/default/downMigration',
-                params:{
-                    module: $scope.activeModule,
-                    fileName:file
-                }
-            }).then(()=>{
-                $scope.getMigrationList($scope.activeModule)
-            })
-        }
+    }
 
-    }])
+    $scope.upMigration = (file) => {
+        $http({
+            method: 'GET',
+            url: '/migrations/default/upMigration',
+            params: {
+                module: $scope.activeModule,
+                fileName: file
+            }
+        }).then(() => {
+            $scope.getMigrationList($scope.activeModule)
+        })
+    }
+    $scope.downMigration = (file) => {
+        $http({
+            method: 'GET',
+            url: '/migrations/default/downMigration',
+            params: {
+                module: $scope.activeModule,
+                fileName: file
+            }
+        }).then(() => {
+            $scope.getMigrationList($scope.activeModule)
+        })
+    }
 
-
+}])
 </script>
 <div ng-app="migration" ng-controller="ctrl">
     <div style="display: flex;margin-top: 20px;">
         <div class=" p-3 bg-white" style="width: 280px;">
 
-                <span class="fs-5 fw-semibold">Модуль миграций</span>
+            <span class="fs-5 fw-semibold">Модуль миграций</span>
 
             <ul class="list-unstyled ps-0">
                 <li class="mb-1">
@@ -98,7 +97,8 @@ Yii::app()->clientScript->registerScriptFile('/protected/modules/modules/assets/
                         <li ng-repeat="module in modulesList" class="">
 
                             <a href="" class="link-dark d-inline-flex text-decoration-none rounded"
-                               ng-class="{'text-bg-primary':module==activeModule}" ng-click="selectModule(module)">{{module}}</a>
+                                ng-class="{'text-bg-primary':module==activeModule}"
+                                ng-click="selectModule(module)">{{module}}</a>
                         </li>
                     </ul>
 
@@ -115,7 +115,7 @@ Yii::app()->clientScript->registerScriptFile('/protected/modules/modules/assets/
                     <div class="input-group">
 
                         <input type="text" class="form-control" placeholder="Название миграции"
-                               ng-model="nameOfNewMigration">
+                            ng-model="nameOfNewMigration">
                     </div>
                 </div>
 
@@ -129,30 +129,32 @@ Yii::app()->clientScript->registerScriptFile('/protected/modules/modules/assets/
             </div>
             <table ng-show="!loading" class="table table-striped">
                 <thead>
-                <tr>
+                    <tr>
 
-                    <th class="col-2">Название</th>
-                    <th class="col-6">Описание</th>
-                    <th class="col-2">Применено</th>
-                    <th class="col-2"></th>
-                </tr>
+                        <th class="col-2">Название</th>
+                        <th class="col-6">Описание</th>
+                        <th class="col-2">Применено</th>
+                        <th class="col-2"></th>
+                    </tr>
                 </thead>
                 <tbody>
-                <tr ng-repeat="migration in migrationList">
+                    <tr ng-repeat="migration in migrationList">
 
-                    <td>{{migration.className}}</td>
-                    <td>{{migration.description}}</td>
-                    <td>
-                        <span ng-if="migration.confirmed">да</span>
-                        <span ng-if="!migration.confirmed">нет</span>
-                    </td>
-                    <td>
-                        <button ng-if="!migration.confirmed" type="button" class="btn btn-success btn-sm" ng-click="upMigration(migration.className)">Применить
-                        </button>
-                        <button ng-if="migration.confirmed" type="button" class="btn btn-danger btn-sm" ng-click="downMigration(migration.className)">Откатить
-                        </button>
-                    </td>
-                </tr>
+                        <td>{{migration.className}}</td>
+                        <td>{{migration.description}}</td>
+                        <td>
+                            <span ng-if="migration.confirmed">да</span>
+                            <span ng-if="!migration.confirmed">нет</span>
+                        </td>
+                        <td>
+                            <button ng-if="!migration.confirmed" type="button" class="btn btn-success btn-sm"
+                                ng-click="upMigration(migration.className)">Применить
+                            </button>
+                            <button ng-if="migration.confirmed" type="button" class="btn btn-danger btn-sm"
+                                ng-click="downMigration(migration.className)">Откатить
+                            </button>
+                        </td>
+                    </tr>
 
 
                 </tbody>
@@ -162,70 +164,62 @@ Yii::app()->clientScript->registerScriptFile('/protected/modules/modules/assets/
 
 </div>
 <style>
+.dropdown-toggle {
+    outline: 0;
+}
 
+.btn-toggle {
+    padding: .25rem .5rem;
+    font-weight: 600;
+    color: rgba(0, 0, 0, .65);
+    background-color: transparent;
+}
 
-    .dropdown-toggle {
-        outline: 0;
-    }
+.btn-toggle:hover,
+.btn-toggle:focus {
+    color: rgba(0, 0, 0, .85);
+    background-color: #d2f4ea;
+}
 
-    .btn-toggle {
-        padding: .25rem .5rem;
-        font-weight: 600;
-        color: rgba(0, 0, 0, .65);
-        background-color: transparent;
-    }
+.btn-toggle::before {
+    width: 1.25em;
+    line-height: 0;
+    content: url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' width='16' height='16' viewBox='0 0 16 16'%3e%3cpath fill='none' stroke='rgba%280,0,0,.5%29' stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M5 14l6-6-6-6'/%3e%3c/svg%3e");
+    transition: transform .35s ease;
+    transform-origin: .5em 50%;
+}
 
-    .btn-toggle:hover,
-    .btn-toggle:focus {
-        color: rgba(0, 0, 0, .85);
-        background-color: #d2f4ea;
-    }
+.btn-toggle[aria-expanded="true"] {
+    color: rgba(0, 0, 0, .85);
+}
 
-    .btn-toggle::before {
-        width: 1.25em;
-        line-height: 0;
-        content: url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' width='16' height='16' viewBox='0 0 16 16'%3e%3cpath fill='none' stroke='rgba%280,0,0,.5%29' stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M5 14l6-6-6-6'/%3e%3c/svg%3e");
-        transition: transform .35s ease;
-        transform-origin: .5em 50%;
-    }
+.btn-toggle[aria-expanded="true"]::before {
+    transform: rotate(90deg);
+}
 
-    .btn-toggle[aria-expanded="true"] {
-        color: rgba(0, 0, 0, .85);
-    }
+.btn-toggle-nav a {
+    padding: .1875rem .5rem;
+    margin-top: .125rem;
+    margin-left: 1.25rem;
+}
 
-    .btn-toggle[aria-expanded="true"]::before {
-        transform: rotate(90deg);
-    }
+.btn-toggle-nav a:hover,
+.btn-toggle-nav a:focus {
+    background-color: #d2f4ea;
+}
 
-    .btn-toggle-nav a {
-        padding: .1875rem .5rem;
-        margin-top: .125rem;
-        margin-left: 1.25rem;
-    }
-
-    .btn-toggle-nav a:hover,
-    .btn-toggle-nav a:focus {
-        background-color: #d2f4ea;
-    }
-
-    .scrollarea {
-        overflow-y: auto;
-    }
-
-
+.scrollarea {
+    overflow-y: auto;
+}
 </style>
 
 <script>
-    /* global bootstrap: false */
-    (() => {
-        'use strict'
-        const tooltipTriggerList = Array.from(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-        tooltipTriggerList.forEach(tooltipTriggerEl => {
-            new bootstrap.Tooltip(tooltipTriggerEl)
-        })
-    })()
-
+/* global bootstrap: false */
+(() => {
+    'use strict'
+    const tooltipTriggerList = Array.from(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    tooltipTriggerList.forEach(tooltipTriggerEl => {
+        new bootstrap.Tooltip(tooltipTriggerEl)
+    })
+})()
 </script>
-
-
-
--- modules/assets/js/modulesService.angular.js
+++ modules/assets/js/modulesService.angular.js
@@ -1,30 +1,44 @@
-angular.module('modules', []).service('modulesService',['$http','$q', function($http,$q) {
-    var modulesList = null;
-    var modulesDataList = null;
+angular.module('modules', []).service('modulesService', [
+	'$http',
+	'$q',
+	function ($http, $q) {
+		var modulesList = null
+		var modulesDataList = null
 
-    this.loadData = function () {
+		this.loadData = function () {
+			var promiseModulesList = $http({
+				method: 'GET',
+				url: '/modules/default/getModulesList',
+			})
+			var promiseModulesDataList = $http({
+				method: 'GET',
+				url: '/modules/default/getModulesDataList',
+			})
 
-        var promiseModulesList = $http({
-            method: 'GET',
-            url: '/modules/default/getModulesList'
-        })
-        var promiseModulesDataList = $http({
-            method:'GET',
-            url:'/modules/default/getModulesDataList'
-        })
+			return $q
+				.all([promiseModulesList, promiseModulesDataList])
+				.then(result => {
+					modulesList = result[0].data
+					modulesDataList = result[1].data
+				})
+		}
 
-        return $q.all([promiseModulesList,promiseModulesDataList]).then((result)=>{
-            modulesList = result[0].data
-            modulesDataList = result[1].data
-        })
-    };
+		this.getModulesList = () => {
+			return modulesList
+		}
 
-    this.getModulesList = ()=>{
-        return modulesList
-    }
-    this.getModulesData = ()=>{
-        return modulesDataList
-    }
-    // factory function body that constructs shinyNewServiceInstance
+		this.getActiveModulesData = () => {
+			console.log(modulesDataList)
+			const activeModules = Object.keys(modulesDataList).filter(
+				key => modulesDataList[key].active
+			)
+			return activeModules
+		}
 
-}]);
+		this.getModulesData = () => {
+			return modulesDataList
+		}
+
+		// factory function body that constructs shinyNewServiceInstance
+	},
+])
