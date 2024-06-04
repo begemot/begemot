b479090841cf9addefe4703d9a11cddea60af9e3
Scott2to <scott2to@gmail.com>
2024-06-04 21:06:45 +0300

    Подключение лейаута пятого бутстрапа на первый экран

--- begemot/controllers/DefaultController.php
+++ begemot/controllers/DefaultController.php
@@ -2,55 +2,53 @@
 
 class DefaultController extends Controller
 {
-    public $layout='begemot.views.layouts.column2';
-    
-    public function actions()
-    {
-        return array(
+	public $layout = 'begemot.views.layouts.bs5clearLayout';
 
-            'fileManager'=>array(
-                'class'=>'begemot.extensions.elfinder.ElFinderAction',
-            ),
-        );
-    }
+	public function actions()
+	{
+		return array(
+
+			'fileManager' => array(
+				'class' => 'begemot.extensions.elfinder.ElFinderAction',
+			),
+		);
+	}
 
 
 	public function actionIndex()
 	{
-        if (isset(Yii::app()->user)&&Yii::app()->user->isGuest){
-		    $this->redirect('/user/login');
-        } else {
-            $this->render('index');
-        }
+
+		if (isset(Yii::app()->user) && Yii::app()->user->isGuest) {
+			$this->redirect('/user/login');
+		} else {
+			$this->render('index');
+		}
 	}
-        
+
 	public function actionLogin()
 	{
-		$model=new BegemotLoginForm;
+		$model = new BegemotLoginForm;
 
 		// if it is ajax validation request
-		if(isset($_POST['ajax']) && $_POST['ajax']==='login-form')
-		{
+		if (isset($_POST['ajax']) && $_POST['ajax'] === 'login-form') {
 			echo CActiveForm::validate($model);
 			Yii::app()->end();
 		}
 
 		// collect user input data
-		if(isset($_POST['BegemotLoginForm']))
-		{
-			$model->attributes=$_POST['BegemotLoginForm'];
+		if (isset($_POST['BegemotLoginForm'])) {
+			$model->attributes = $_POST['BegemotLoginForm'];
 			// validate user input and redirect to the previous page if valid
-			if($model->validate() && $model->login())
+			if ($model->validate() && $model->login())
 				$this->redirect('/admin');
 		}
 		// display the login form
-		$this->render('index',array('model'=>$model));
-	}     
-        
+		$this->render('index', array('model' => $model));
+	}
+
 	public function actionLogout()
 	{
 		Yii::app()->user->logout();
 		$this->redirect('/begemot');
-	}        
-        
+	}
 }
