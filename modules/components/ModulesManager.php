3268df0156294fdae0e6e464270b5aefcb10868a
Scott2to <scott2to@gmail.com>
2024-06-06 16:26:20 +0300

    Функция проверки активности модуля.

--- modules/components/ModulesManager.php
+++ modules/components/ModulesManager.php
@@ -11,26 +11,26 @@ class ModulesManager
         'pictureBox' => '',
         'RolesImport' => '',
         'user' => '',
-        'srbac'=>'',
-        'pictureBox'=>'',
-        'settings'=>''
+        'srbac' => '',
+        'pictureBox' => '',
+        'settings' => ''
 
     ];
 
     public static function isModuleByDefault($module)
     {
-        return isset (self::DEFAULT_MODULES[$module]);
+        return isset(self::DEFAULT_MODULES[$module]);
     }
 
 
     public static function getModulesData()
     {
-       // Yii::import('begemot.extensions.vault.FileVault');
+        // Yii::import('begemot.extensions.vault.FileVault');
         $dir = dirname(__FILE__);
-        $vaultClassPath = $dir.'/../../begemot/extensions/vault/FileVault.php';
+        $vaultClassPath = $dir . '/../../begemot/extensions/vault/FileVault.php';
 
         require_once $vaultClassPath;
-        $vaultPath = $dir.'/../../../../files/modules_data';
+        $vaultPath = $dir . '/../../../../files/modules_data';
 
         if (!file_exists($vaultPath))
             mkdir($vaultPath, 0777, true);
@@ -54,10 +54,9 @@ class ModulesManager
                     $data[$moduleName]['active'] = false;
                 }
             }
-
         }
 
-        self::saveModulesData($data);
+       self::saveModulesData($data);
         return $data;
     }
 
@@ -65,12 +64,12 @@ class ModulesManager
     {
 
         $dir = dirname(__FILE__);
-        $vaultClassPath = $dir.'/../../begemot/extensions/vault/FileVault.php';
+        $vaultClassPath = $dir . '/../../begemot/extensions/vault/FileVault.php';
 
         require_once $vaultClassPath;
 
         $dir = __DIR__;
-        $vaultPath = $baseWebDir = $dir.'/../../../../files/modules_data';
+        $vaultPath = $baseWebDir = $dir . '/../../../../files/modules_data';
         //$vaultPath = Yii::getPathOfAlias('webroot.files.modules_data');
         if (!file_exists($vaultPath))
             mkdir($vaultPath, 0777, true);
@@ -78,14 +77,13 @@ class ModulesManager
         $modulesDataVault = new FileVault($vaultPath);
 
         $modulesDataVault->pushCollection($data);
-
     }
 
     public static function getModulesList()
     {
-//        $modulesPath = Yii::getPathOfAlias('application.modules');
-       // die($modulesPath);
-        $modulesPath = dirname(__FILE__).'/../../';
+        //        $modulesPath = Yii::getPathOfAlias('application.modules');
+        // die($modulesPath);
+        $modulesPath = dirname(__FILE__) . '/../../';
         $modulesDirs = glob($modulesPath . '/*', GLOB_ONLYDIR);
         $modulesList = [];
         foreach ($modulesDirs as $moduleDir) {
@@ -95,4 +93,11 @@ class ModulesManager
         return $modulesList;
     }
 
+    public static function isModuleActive($moduleName)
+    {
+        $data = self::getModulesData();
+        if (isset($data[$moduleName]['active'])) {
+            return $data[$moduleName]['active'];
+        } else return false;
+    }
 }
