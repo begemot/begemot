b3d109c3c8c8576549de9c2d4d1574fda1be235a
Scott2to <scott2to@gmail.com>
2024-07-17 13:30:38 +0300

    мелочи

--- catalog/views/catItemOptions/js/manage.angular.js
+++ catalog/views/catItemOptions/js/manage.angular.js
@@ -4,14 +4,14 @@ app.controller('myCtrl', function ($scope, $http) {
     $scope.selectedItems = [];
     $scope.celectedItem = undefined
     // Функция, которая будет вызываться при изменении selectedItems
-    $scope.onSelectItem = function(item) {
+    $scope.onSelectItem = function (item) {
         console.log('Первый элемент selectedItems:', item);
         $scope.selectedItem = item
         // Здесь можно добавить дополнительную логику для обработки элемента item
     };
 
     // Функция, которая будет вызываться, когда selectedItems становится пустым
-    $scope.deselectItem = function() {
+    $scope.deselectItem = function () {
         console.log('selectedItems пустой');
         // Здесь можно добавить дополнительную логику, которая должна выполняться, когда selectedItems становится пустым
     };
